c15755d3fbbb8d806641b43d0d7b633aa9f7061c
KGANESH2003 <ganeshesrs2266@gmail.com>
2025-03-06 17:38:44 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.15%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
         int res=0;
         int i=0;
         while(i<nums.size())
         {
             res^=nums[i];
             i++;
-        }return res;
+        }
+        return res;
     }
 };
