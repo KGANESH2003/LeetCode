a80fa73f13b933f8a837c08522cc0dabf63c07ae
KGANESH2003 <ganeshesrs2266@gmail.com>
2025-03-27 10:44:43 +0530

    Time: 0 ms (100%), Space: 11.5 MB (73.36%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int start = 0, middle = 0, end = nums.size() - 1;
-
-        while (middle <= end) {
-            if (nums[middle] == 0) {
-                swap(nums[start], nums[middle]);
+        int start=0,middle=0,end=nums.size()-1;
+        while(middle<=end)
+        {
+            if(nums[middle]==0)
+            {
+                swap(nums[start],nums[middle]);
                 start++;
                 middle++;
-            } else if (nums[middle] == 2) {
-                swap(nums[middle], nums[end]);
+            }
+            else if(nums[middle]==2)
+            {
+                swap(nums[middle],nums[end]);
                 end--;
-            } else {
+            }
+            else{
                 middle++;
             }
         }
