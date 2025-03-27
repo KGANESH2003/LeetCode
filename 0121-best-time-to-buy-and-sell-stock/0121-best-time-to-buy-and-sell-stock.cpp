13533c8680aa5ba573b71e89eba9e5281bd83f73
KGANESH2003 <ganeshesrs2266@gmail.com>
2025-03-27 11:08:50 +0530

    Time: 0 ms (100%), Space: 97.5 MB (25.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,10 +4,12 @@ public:
         int minPrice = INT_MAX;  // To track the minimum price encountered so far
         int maxProfit = 0;      // To track the maximum profit
 
-        for (int price : prices) {
-            minPrice = min(minPrice, price);  // Update the minimum price
-            maxProfit = max(maxProfit, price - minPrice);  // Update the maximum profit
+        for(int i=0;i<prices.size();i++)
+        {
+            minPrice=min(minPrice,prices[i]);
+            maxProfit=max(maxProfit,prices[i]-minPrice);
         }
+        
 
         return maxProfit;
     }
