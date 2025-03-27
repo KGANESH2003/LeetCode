95fd2ff34813556fad5b5220f30f6b9d684fc22c
KGANESH2003 <ganeshesrs2266@gmail.com>
2025-03-27 10:40:17 +0530

    Time: 0 ms (100%), Space: 28.4 MB (6.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> intcount;
-
-        // Count frequency of each number
-        for(int num : nums) {
+        unordered_map<int,int>intcount;
+        for(int num:nums)
+        {
             intcount[num]++;
         }
-
-        // Find the majority element
-        for(int num : nums) {
-            if(intcount[num] > nums.size() / 2) {
-                return num;  // Return the majority element
+        for(int num:nums)
+        {
+            if(intcount[num]>nums.size()/2)
+            {
+                return num;
             }
-        }
+        }return -1;
         
-        // This return is just a placeholder; the problem guarantees a majority element exists.
-        return -1; 
     }
 };
