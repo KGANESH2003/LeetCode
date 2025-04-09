b79251d2a1d15f557d869d103b3a45e79ca0bbfa
KGANESH2003 <ganeshesrs2266@gmail.com>
2025-04-09 11:24:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (39.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,51 @@
 class Solution {
+private:
+    void swapifgreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2) {
+        if (nums1[ind1] > nums2[ind2]) {
+            swap(nums1[ind1], nums2[ind2]);
+        }
+    }
+
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> res;
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
 
-        // Copy only the valid elements from nums1
-        for (int i = 0; i < m; i++) {
-            res.push_back(nums1[i]);
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // Case 1: both pointers in nums1
+                if (left < m && right < m) {
+                    if (nums1[left] > nums1[right]) {
+                        swap(nums1[left], nums1[right]);
+                    }
+                }
+                // Case 2: left in nums1, right in nums2
+                else if (left < m && right >= m) {
+                    if (nums1[left] > nums2[right - m]) {
+                        swap(nums1[left], nums2[right - m]);
+                    }
+                }
+                // Case 3: both pointers in nums2
+                else {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        swap(nums2[left - m], nums2[right - m]);
+                    }
+                }
+
+                left++;
+                right++;
+            }
+
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
 
-        // Copy all elements from nums2
-        for (int j = 0; j < n; j++) {
-            res.push_back(nums2[j]);
-        }
-
-        // Sort the merged array
-        sort(res.begin(), res.end());
-
-        // Copy the sorted elements back to nums1
-        for (int i = 0; i < res.size(); i++) {
-            nums1[i] = res[i];
+        // Copy nums2 into nums1 at the end
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 };
